6a61c4eec60daf46d867049b9697f2038fb38362
NehalRajput <nehal17.ast@gmail.com>
2025-03-06 16:51:52 +0530

    How to Preview and Send Email Using Mailable Classes

--- web/routes/web.php
+++ web/routes/web.php
@@ -6,6 +6,7 @@ use App\Http\Controllers\RegisteredUserController;
 use App\Http\Controllers\SessionController;
 use Illuminate\Support\Facades\Route;
 
+
 Route::view('/', 'home');
 Route::view('/contact', 'contact');
 
